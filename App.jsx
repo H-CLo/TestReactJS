6355c800aa90d1d44c4c4459d15704280a893261
Hung Chang Lo <nba1895@gmail.com>
2018-01-07 17:56:08 +0800

    Day Six

---
+++ App.jsx
@@ -0,0 +1,54 @@
+// App.jsx
+// 用於引入要使用的函式庫或程式碼檔案，與 Require（ES5語法）相同，import from為ES6+的寫法。
+// Export 匯出，可以被視作是將程式碼做為一個組件的設定匯出，方便之後在其他地方重複使用（reuse）。
+import React from 'react';
+class Header extends React.Component {
+   render() {
+      return (
+          <h1>Header</h1>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
+         </div>
+      );
+   }
+}
+
+/* 組件具有物件的概念，extend是繼承的意思，讓產生出來的組件具備一些組件可以執行的函式，像是下面的render。*/
+class App extends React.Component {
+
+    constructor(){
+   	    super();
+        this.state={
+            "name":"WilleyLo"
+        }
+    }
+   /*渲染，也就是將JSX所寫的網頁元素繪製出來的函式。要特別注意的是render一次只能產生一個標籤，若有複數個標籤共同組成一個組件，需要用巢狀的結構去進行渲染。*/
+    render() {
+   	  /*駝峰式語法*/
+   	  var myStyle = {
+        fontSize: 100,
+        color: '#FF0000',
+      }
+
+   	  var i = 1;
+   	  /* Return JSX 的XML-like語法 */
+      return (
+         <div>
+            <h1>{i = 1 ? 'True' : 'False'}</h1>
+            <h2 style = {myStyle}>header2</h2>
+            <Header/>
+            <Content/>
+            <p>made by {this.state.name}</p>
+         </div>
+      );
+   }
+}
+export default App;
